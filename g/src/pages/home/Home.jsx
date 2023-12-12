78535bfffcaf8b50450ef175464119814af8d8b1
Sattorov Kamronbek <pubggameruzb.sila@gmail.com>
2023-12-12 21:14:57 +0500

    project the end

---
+++ g/src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Navbar } from "../../components/pages/home-component";
+
+export const Home = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
